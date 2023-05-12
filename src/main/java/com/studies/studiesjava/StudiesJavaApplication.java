f580b58634cd9952bcfdcbd565717dcdb9ef8838
Rogério Brito <rogerio_brito@sicredi.com.br>
2023-05-12 18:27:00 -0300

    Commit inicial

---
+++ src/main/java/com/studies/studiesjava/StudiesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.studies.studiesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesJavaApplication.class, args);
+	}
+
+}
